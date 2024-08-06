b35e83621633e16b6ab6adfd75a3bc9433e06f52
srocknik <somesh.chutel90@gmail.com>
2024-08-06 18:25:56 +0530

    second commit

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,13 +1,78 @@
-import './index.css'
+import { useState } from "react";
+import "./index.css";
 
-const Login =()=>{
+const Login = () => {
+
+    const [allValues,setValues] = useState({
+        username : "",
+        password : ""
+    });
+
+    const onSubmitUserDetails = async(e)=>{
+        e.preventDefault();
+
+        const api = "https://apis.ccbp.in/login";
+
+        const userDetails = {
+            username: allValues.username,
+            password: allValues.password
+        }
+
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(userDetails)
+        }
+
+        const response = await fetch(api,options);
+        const data = await response.json(); 
+
+        console.log(data);
+        
+    }
+
+    const onChangeUserName = (e)=>{
+
+        setValues({...allValues,username : e.target.value});
+
+    }
 
 
-    return (
-        <h1> Login Component </h1>
-    )
-}
+    const onChangePassword = (e)=>{
 
+        setValues({...allValues,password : e.target.value});
+    }
 
+  return (
+    <div className="login-cont">
+      <form className="my-form" onSubmit={onSubmitUserDetails}>
+        <div className="login-web-logo-cont">
+        <img src="https://assets.ccbp.in/frontend/react-js/logo-img.png" className="w-25"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={onChangeUserName}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            onChange={onChangePassword}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
